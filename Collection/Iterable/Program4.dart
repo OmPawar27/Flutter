d93454fb4ed6a2daff2097979f7a80371d099f0c
Om Pawar <ompawar4242@gmail.com>
2023-11-05 18:57:03 -0800

    Program4 added

---
+++ Collection/Iterable/Program4.dart
@@ -0,0 +1,9 @@
+// elementAt(int index) -> E
+// return Type : Element
+
+void main(){
+    var players = ["Rohit","Shubman","Virat","KLRahul","Shreyas","Hardik"];
+    
+    var retVal1 = players.elementAt(2);
+    print(retVal1);     // Virat
+}
