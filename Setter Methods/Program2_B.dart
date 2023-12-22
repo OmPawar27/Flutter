df0958404d4edc4485d440d240e8da3c402a525f
Om Pawar <ompawar4242@gmail.com>
2023-12-21 22:42:14 -0800

    Initial Commit

---
+++ Setter Methods/Program2_B.dart
@@ -0,0 +1,12 @@
+import 'Program2_A.dart';
+
+void main() {
+  Demo obj = new Demo(10, "Om", 1.5);
+  obj.disp();
+
+  obj.setID = 20;
+  obj.setName = "Subhaan";
+  obj.setSalary = 2.0;
+
+  obj.disp();
+}
