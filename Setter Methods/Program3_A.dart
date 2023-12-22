33f924ecaef74a6c08fd72500c29a9a62b720bbe
Om Pawar <ompawar4242@gmail.com>
2023-12-21 22:42:27 -0800

    Initial Commit

---
+++ Setter Methods/Program3_A.dart
@@ -0,0 +1,17 @@
+class Demo {
+  int? _id;
+  String? name;
+  double? _salary;
+
+  Demo(this._id, this.name, this._salary);
+
+  set setID(int id) => _id = id;
+
+  set setName(String name) => this.name = name;
+
+  set setSalary(double salary) => _salary = salary;
+
+  void disp() {
+    print("$_id $name $_salary");
+  }
+}
