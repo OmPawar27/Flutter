fac9e3c0553e443dd002183bfcb2590a59b7e26d
Om Pawar <ompawar4242@gmail.com>
2023-12-22 22:51:32 -0800

    Initial commit

---
+++ Null Safety/NullSafety1.dart
@@ -0,0 +1,16 @@
+// ? Allows you to store null values or actual values 
+
+void main(){
+    
+    int? age = null;
+    String? name = "Om";
+
+    print(age);
+    print(name);
+
+    age = 21;
+    name = null;
+
+    print(age);
+    print(name);
+}
