6548c788529deddbfba78c4c26a783b928886d3b
Om Pawar <ompawar4242@gmail.com>
2023-12-26 22:03:14 -0800

    Inital Commit

---
+++ Constructors/Parameterized Constructor/Program1.dart
@@ -0,0 +1,12 @@
+class Demo {
+  Demo(int parameter) {
+    print("In Parameterized Constructor");
+    print(parameter);
+  }
+}
+
+void main() {
+  Demo(10);
+  //In Parameterized Constructor
+  //10
+}
